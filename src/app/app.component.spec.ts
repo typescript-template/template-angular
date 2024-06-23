d2b6b3057bd6bd0b83e16ae603088d0822e46c35
Drew Immerman <drewimmerman@gmail.com>
2024-06-23 15:40:21 -0400

    Fix project name variables

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	});
 
-	it('should have the \'angular-template-ng\' title', () => {
+	it('should have the \'{{project-name}}\' title', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.componentInstance;
-		expect(app.title).toEqual('angular-template-ng');
+		expect(app.title).toEqual('{{project-name}}');
 	});
 
 	it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement as HTMLElement;
 		expect(compiled.querySelector('h1')?.textContent).toContain(
-			'Hello, angular-template-ng'
+			'Hello, {{project-name}}'
 		);
 	});
 });
